171d739da1772d22023a108bc11ed31bf2097fd7
Adam Whaley <abwn83@gmail.com>
2025-10-31 10:41:01 -0500

    fixing tests again

--- src/app/pants.test.tsx
+++ src/app/pants.test.tsx
@@ -1,20 +1,20 @@
-import {render, screen} from "@testing-library/react";
+import {act, render, screen} from "@testing-library/react";
 import {Pants} from "@/app/pants";
-import fetchMock from 'jest-fetch-mock';
-
+import fetchMock, { FetchMock} from 'jest-fetch-mock';
 
 describe('Pants', () => {
     describe('base scenario', () => {
         beforeEach(() => {
             fetchMock.resetMocks(); // Clear mocks before each test
-            fetchMock.mockResolvedValue({ data: 'mocked data' });
-
-            render(<Pants />);
-
+            fetchMock.mockResponse(JSON.stringify({ data: 'mocked data' }));
         });
-        it('renders ', () => {
 
-            const ele = screen.findByText('mocked data');
+        it('renders ', async () => {
+            act(() => {
+                render(<Pants/>);
+            })
+
+            const ele = await screen.findByText('mocked data');
             expect(ele).toBeDefined();
         });
 
--- src/app/pants.tsx
+++ src/app/pants.tsx
@@ -1,15 +1,14 @@
 import {useEffect, useState} from "react";
 
 export  function Pants() {
-    const [foo,setFoo] = useState();
+    const [foo,setFoo] = useState('');
     useEffect( () => {
         const func = async () => {
             const data = await fetch("http://localhost")
-            setFoo(await data.json());
+            setFoo((await data.json()).data);
 
         };
         func();
-
     })
 
     return <p>{foo}</p>;
